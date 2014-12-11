bc3ed53b99538f0c3e73e307a4417d10b5ada35f
Ricardo de pena <ricardo.depena@gmail.com>
2014-12-11 17:01:48 -0500

    added the lib folder to the package.json and added a noop callback as a default.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var exec = require('child_process').exec,
         rvmPath: path.resolve('OpenFinRVM.exe'),
         rvmUrl: 'https://developer.openfin.co/release/rvm/latest'
     },
-    callback;
+    callback = function () {};
 
 function launchOpenFin(options, cb) {
-    callback = callback || cb;
+    callback = cb || callback;
     //check if we are in windows.
     _.extend(defaultOptions, options);
     if (os.type().toLowerCase().indexOf('windows') > -1) {
